1e4b2dae3bfba3333a458f70ecc581e9bec3a8c2
hepalma <he.palma@duocuc.cl>
2023-09-27 02:17:44 -0300

    home + habitaciones v 1.0

--- webhotel/core/views.py
+++ webhotel/core/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render, redirect
 
 def home(request):
     return render(request, 'app/home.html')
+
+def habitaciones(request):
+    return render(request, 'app/habitaciones.html')
