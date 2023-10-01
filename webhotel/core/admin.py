8b8ca55448f6f48626e70e0d44a6f1edece211b6
Jose Falfan <jose.falfan1@gmail.com>
2023-10-01 19:14:16 -0300

    modelos admin listos

--- webhotel/core/admin.py
+++ webhotel/core/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
-
+from .models import cliente, hotel, habitacion, reserva
 # Register your models here.
+
+class ClientSetting(admin.ModelAdmin):
+    list_display = ["idCliente", "nombre", "apellido", "correo", "telefono"]
+    list_editable = ["nombre", "apellido", "correo", "telefono"]
+    list_filter = ["idCliente", "nombre"]
+
+
+class HotelSetting(admin.ModelAdmin):
+    list_display = ["idHotel", "nombreHotel", "direccion", "categoria"]
+    list_editable = ["nombreHotel", "direccion", "categoria"]
+
+
+class HabitacionSetting(admin.ModelAdmin):
+    list_display = ["idHabitacion", "idHotel", "tipoHabitacion", "capacidadHabitacion", "precioHabitacion", "descripcionHabitacion", "estadoHabitacion"]
+    list_editable = ["tipoHabitacion", "capacidadHabitacion", "precioHabitacion", "descripcionHabitacion", "estadoHabitacion"]
+    list_filter = ["tipoHabitacion", "capacidadHabitacion", "precioHabitacion", "estadoHabitacion"]
+
+
+
+class ReservaSetting(admin.ModelAdmin):
+    list_display = ["idReserva", "idHabitacion", "idCliente", "fechaArribo", "fechaPartida", "cantidadPersonas"]
+    list_editable = ["fechaArribo", "fechaPartida", "cantidadPersonas"]
+    list_filter = ["fechaArribo", "fechaPartida", "cantidadPersonas"]
+
+
+
+admin.site.register(reserva, ReservaSetting)
+admin.site.register(habitacion, HabitacionSetting)
+admin.site.register(hotel, HotelSetting)
+admin.site.register(cliente, ClientSetting)
--- webhotel/core/models.py
+++ webhotel/core/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+class cliente(models.Model):
+    idCliente= models.AutoField(primary_key=True, verbose_name="Id cliente")
+    nombre = models.CharField(max_length=50, verbose_name="Nombre cliente")
+    apellido = models.CharField(max_length=50, verbose_name="apellido cliente")
+    correo = models.CharField(max_length=50, verbose_name="Correo cliente")
+    telefono = models.IntegerField(max_length=9, verbose_name="Telefono cliente")
+
+    def __str__(self):
+        return self.idCliente
+    
+
+
+class hotel(models.Model):
+    idHotel= models.AutoField(primary_key=True, verbose_name="Id hotel")
+    nombreHotel = models.CharField(max_length=50, verbose_name="Nombre hotel")
+    direccion = models.CharField(max_length=50, verbose_name="Direccion hotel")
+    categoria = models.CharField(max_length=50, verbose_name="Categoria Hotel")
+
+    def __str__(self):
+        return self.idHotel
+
+
+
+
+class habitacion(models.Model):
+    idHabitacion= models.AutoField(primary_key=True, verbose_name="Id habitacion")
+    idHotel = models.ForeignKey(hotel, on_delete=models.CASCADE)
+    tipoHabitacion = models.CharField(max_length=50, verbose_name="tipo habitacion")
+    capacidadHabitacion = models.CharField(max_length=50, verbose_name="Capacidad habitacion")
+    precioHabitacion = models.CharField(max_length=50, verbose_name="precio habitacion")
+    descripcionHabitacion = models.TextField(max_length=2000, verbose_name="descripcion")
+    estadoHabitacion = models.CharField(max_length=50, verbose_name="estado habitacion")
+
+    def __str__(self):
+        return self.idHabitacion
+
+
+class reserva(models.Model):
+    idReserva= models.AutoField(primary_key=True, verbose_name="Id reserva")
+    idHabitacion = models.ForeignKey(habitacion, on_delete=models.CASCADE)
+    idCliente = models.ForeignKey(cliente, on_delete=models.CASCADE)
+    fechaArribo = models.DateField(verbose_name="Fecha arribo")
+    fechaPartida = models.DateField(verbose_name="Fecha salida")
+    cantidadPersonas = models.CharField(max_length=50, verbose_name="Cantidad personas")
+
+    def __str__(self):
+        return self.idHabitacion
