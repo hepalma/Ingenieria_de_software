8c00c5d7c40d9050fa91612f8c2ddc1b070598cf
Jose Falfan <jose.falfan1@gmail.com>
2023-10-01 23:56:59 -0300

    commit bueno

--- webhotel/core/views.py
+++ webhotel/core/views.py
@@ -6,11 +6,24 @@ from .forms import ReservaForm
 def home(request):
     return render(request, 'app/home.html')
 
+
 def habitaciones(request):
     habitaciones = habitacion.objects.all()
     data = { 'habitaciones' : habitaciones}
     return render(request, 'app/habitaciones.html', data)
 
+
 def reserva(request):
     data = { 'form': ReservaForm()}
+    if request.method == 'POST':
+        formulario = ReservaForm(data=request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            data ["mensaje"] = "Solicitud de reserva enviada."
+        else:
+            data['form'] = formulario
     return render(request, 'app/reserva.html', data)
+
+
+def contacto(request):
+    return render(request, 'app/contacto.html')
